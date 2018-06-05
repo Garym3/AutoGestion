7f443f4b8d1ede639bf8d97c602298579fd3fdfe
olefebvre <olivier.lefebvre@eurotitrisation.fr>
2018-06-05 16:54:39 +0200

    Comments cleanup

--- AutoParcGestion/Vehicles/Builder/ICarBuilder.cs
+++ AutoParcGestion/Vehicles/Builder/ICarBuilder.cs
@@ -1,20 +1,7 @@
-﻿using AutoGestion.Vehicles.Template;
-using static AutoGestion.Vehicles.Utils.VehicleEnums;
-
-namespace AutoGestion.Vehicles.Builder
+﻿namespace AutoGestion.Vehicles.Builder
 {
     public interface ICarBuilder : IVehicleBuilder<ICarBuilder>
     {
-        /*Vehicle Build();
-
-        ICarBuilder WithBrand(Brands brand);
-
-        ICarBuilder WithColor(Colors color);
-
-        ICarBuilder WithEngineCapacity(int engineCapacity);
-
-        ICarBuilder WithDoors(int numberOfDoors);
-
-        ICarBuilder WithSeats(int numberOfSeats);*/
+        
     }
 }
--- AutoParcGestion/Vehicles/Builder/ITruckBuilder.cs
+++ AutoParcGestion/Vehicles/Builder/ITruckBuilder.cs
@@ -2,18 +2,6 @@
 {
     public interface ITruckBuilder : IVehicleBuilder<ITruckBuilder>
     {
-        /*Vehicle Build();
-
-        ITruckBuilder WithBrand(Brands brand);
-
-        ITruckBuilder WithColor(Colors color);
-
-        ITruckBuilder WithEngineCapacity(int engineCapacity);
-
-        ITruckBuilder WithDoors(int numberOfDoors);
-
-        ITruckBuilder WithSeats(int numberOfSeats);*/
-
         ITruckBuilder WithFreight(double freight);
     }
 }
